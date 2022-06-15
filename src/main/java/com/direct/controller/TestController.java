efbe9fa05d1f90e70fc48f2a058dfebb69a7f34c
admin <admin@gjy.com>
2022-06-15 14:11:53 +0800

    seconde commit

--- src/main/java/com/direct/controller/TestController.java
+++ src/main/java/com/direct/controller/TestController.java
@@ -3,5 +3,6 @@ package com.direct.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("git1.......");
+        System.out.println("git2..........");
     }
 }
